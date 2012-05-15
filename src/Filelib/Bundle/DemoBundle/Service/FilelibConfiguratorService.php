35dcf7c893b68f8368386761e4a80b380498ce9b
Mikko Forsström <puhemies@diktaattoriporssi.com>
2012-05-15 16:38:21 +0300

    Made demo utilize filelib hooks to inject random data for test plugin.

---
+++ src/Filelib/Bundle/DemoBundle/Service/FilelibConfiguratorService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Filelib\Bundle\DemoBundle\Service;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Xi\Filelib\Event\FilelibEvent;
+use Xi\Filelib\Event\FileEvent;
+
+class FilelibConfiguratorService implements EventSubscriberInterface
+{
+
+    /**
+     * Returns an array of subscribed events
+     *
+     * @return array
+     */
+    static public function getSubscribedEvents()
+    {
+        return array('file.instantiate' => 'onFileInstantiate');
+    }
+
+
+    public function onFileInstantiate(FileEvent $event)
+    {
+        $file = $event->getFile();
+
+        $data = $file->getData();
+
+        $width = rand(200, 800);
+        $height = rand(200, 800);
+
+        $data['plugin.testplugin'] = array($width, $height, false);
+
+    }
+
+
+}
